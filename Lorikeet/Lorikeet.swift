7695bd09c7d120cc7850a5aaeb96b0e1d3abb093
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2017-11-03 22:04:39 +0000

    Tweek complimentaryColor brightness

--- Lorikeet/Lorikeet.swift
+++ Lorikeet/Lorikeet.swift
@@ -66,7 +66,7 @@ public struct Lorikeet {
         
         let newHue = (self.hue + 180/360) - 1.0
 
-        let complimentaryBrightness: CGFloat = self.brightnessFactor >= 0.7 ? 0.3 : 1.0
+        let complimentaryBrightness: CGFloat = self.brightnessFactor < 0.9 ? 1.0 : 0.0
 
         self.complimentaryColor = UIColor(hue: newHue,
                                           saturation: self.saturation,
